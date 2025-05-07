2773b6dae70df5c9ea5db7437c9bb7ddf86515c5
viktorashi <ioanvictorstan@gmail.com>
2025-05-07 10:55:48 +0300

    astea de safari

--- src/test/java/org/example/features/search/SearchByKeywordStory.java
+++ src/test/java/org/example/features/search/SearchByKeywordStory.java
@@ -3,14 +3,11 @@ package org.example.features.search;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Issue;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Pending;
 import net.thucydides.core.annotations.Steps;
-
+import org.example.steps.serenity.EndUserSteps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-
-import org.example.steps.serenity.EndUserSteps;
 
 @RunWith(SerenityRunner.class)
 public class SearchByKeywordStory {
